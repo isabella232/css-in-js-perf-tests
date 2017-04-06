207aaedcbd93308f6ff82e0ca5d6e5e18d04808c
Sunil Pai <threepointone@gmail.com>
2017-04-06 18:20:20 +0530

    calculate nested styles just once

--- src/nested-test/cases/glamor.js
+++ src/nested-test/cases/glamor.js
@@ -3,9 +3,12 @@ import { style } from 'glamor';
 import { createContainerStyle, createButtonStyle } from '../styles';
 import { renderHtml, renderBody } from '../render';
 
+const containerStyle = createContainerStyle()
+const buttonStyle = createButtonStyle()
+
 export const glamorCase = (caseName) => {
     const { html, css } = renderStatic(() =>
-        renderBody(caseName, style(createContainerStyle()), style(createButtonStyle()))
+        renderBody(caseName, style(containerStyle), style(buttonStyle))
     );
 
     return renderHtml(css, html);
